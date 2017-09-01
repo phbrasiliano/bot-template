56c3c758963f7640a95a771fa8c85e917fed203c
Phellipe <phbrasiliano@gmail.com>
2017-09-01 20:39:03 +0200

    removes deprecated function and commented lines

--- SoundFinder.py
+++ SoundFinder.py
@@ -1,5 +1,3 @@
-# import urllib.request
-# import urllib.parse
 import urllib
 import re
 from bs4 import BeautifulSoup
@@ -20,26 +18,6 @@ def reply_sound(bot, update, args):
             update.message.reply_text("Não encontrei nada %s" % ("relacionado a " + term))
     except (IndexError, ValueError):
         update.message.reply_text('Use da seguinte forma: /sound "termo a ser pesquisado"')
-
-
-def sound_finder_old(term):
-    class AppURLopener(urllib.request.FancyURLopener):
-        version = "Mozilla/5.0"
-    term = urllib.parse.quote(term)
-    opener = AppURLopener()
-    screen1 = (opener.open('https://www.myinstants.com/search/?name=%s' % term)).read()
-    m = re.search('/instant/(.+?)/', str(screen1))
-    if m:
-        key1 = m.group(1)
-        screen2 = opener.open('https://www.myinstants.com/instant/%s/' % key1).read()
-        m = re.search('href="/media/sounds(.+?)mp3" d', str(screen2))
-        if m:
-            key2 = m.group(1)
-            final_url = 'https://www.myinstants.com/media/sounds/%smp3' % key2
-            print('audio link: ', final_url)
-            final_file = opener.open(final_url)
-            return final_file
-
 
 '''this function returns a list with a maximum of 5 sounds,
  each item contain a tuple with a sound name and their url address'''
